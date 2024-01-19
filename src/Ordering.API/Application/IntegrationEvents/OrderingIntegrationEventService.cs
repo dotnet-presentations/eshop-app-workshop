3b978104f61bbea88af6c26c59dcc8baf999b583
Damian Edwards <damian@damianedwards.com>
2024-01-18 17:49:34 -0800

    Add initial "working" slimmed down eShop

---
+++ src/Ordering.API/Application/IntegrationEvents/OrderingIntegrationEventService.cs
@@ -0,0 +1,44 @@
+﻿namespace eShop.Ordering.API.Application.IntegrationEvents;
+
+public class OrderingIntegrationEventService(IEventBus eventBus,
+    OrderingContext orderingContext,
+    //IIntegrationEventLogService integrationEventLogService,
+    ILogger<OrderingIntegrationEventService> logger) : IOrderingIntegrationEventService
+{
+    private readonly IEventBus _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+    private readonly OrderingContext _orderingContext = orderingContext ?? throw new ArgumentNullException(nameof(orderingContext));
+    //private readonly IIntegrationEventLogService _eventLogService = integrationEventLogService ?? throw new ArgumentNullException(nameof(integrationEventLogService));
+    private readonly ILogger<OrderingIntegrationEventService> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public Task PublishEventsThroughEventBusAsync(Guid transactionId)
+    {
+        return Task.CompletedTask;
+
+        //var pendingLogEvents = await _eventLogService.RetrieveEventLogsPendingToPublishAsync(transactionId);
+
+        //foreach (var logEvt in pendingLogEvents)
+        //{
+        //    _logger.LogInformation("Publishing integration event: {IntegrationEventId} - ({@IntegrationEvent})", logEvt.EventId, logEvt.IntegrationEvent);
+
+        //    try
+        //    {
+        //        //await _eventLogService.MarkEventAsInProgressAsync(logEvt.EventId);
+        //        await _eventBus.PublishAsync(logEvt.IntegrationEvent);
+        //        //await _eventLogService.MarkEventAsPublishedAsync(logEvt.EventId);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, "Error publishing integration event: {IntegrationEventId}", logEvt.EventId);
+
+        //        //await _eventLogService.MarkEventAsFailedAsync(logEvt.EventId);
+        //    }
+        //}
+    }
+
+    public Task AddAndSaveEventAsync(IntegrationEvent evt)
+    {
+        _logger.LogInformation("Enqueuing integration event {IntegrationEventId} to repository ({@IntegrationEvent})", evt.Id, evt);
+        return Task.CompletedTask;
+        //await _eventLogService.SaveEventAsync(evt, _orderingContext.GetCurrentTransaction());
+    }
+}
---
+++ src/WebApp/Extensions/Extensions.cs
@@ -0,0 +1,101 @@
+﻿using eShop.WebApp;
+using eShop.WebAppComponents.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+public static class Extensions
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        //builder.AddAuthenticationServices();
+
+        //builder.AddRabbitMqEventBus("EventBus")
+        //       .AddEventBusSubscriptions();
+
+        builder.Services.AddHttpForwarderWithServiceDiscovery();
+
+        // Application services
+        builder.Services.AddScoped<BasketState>();
+        builder.Services.AddScoped<LogOutService>();
+        builder.Services.AddSingleton<BasketService>();
+        builder.Services.AddSingleton<OrderStatusNotificationService>();
+        builder.Services.AddSingleton<IProductImageUrlProvider, ProductImageUrlProvider>();
+
+        // HTTP and GRPC client registrations
+        builder.Services.AddGrpcClient<Basket.BasketClient>(o => o.Address = new("http://basket-api"))
+            .AddAuthToken();
+
+        builder.Services.AddHttpClient<CatalogService>(o => o.BaseAddress = new("http://catalog-api"))
+            .AddAuthToken();
+
+        builder.Services.AddHttpClient<OrderingService>(o => o.BaseAddress = new("http://ordering-api"))
+            .AddAuthToken();
+    }
+
+    //public static void AddEventBusSubscriptions(this IEventBusBuilder eventBus)
+    //{
+    //    eventBus.AddSubscription<OrderStatusChangedToAwaitingValidationIntegrationEvent, OrderStatusChangedToAwaitingValidationIntegrationEventHandler>();
+    //    eventBus.AddSubscription<OrderStatusChangedToPaidIntegrationEvent, OrderStatusChangedToPaidIntegrationEventHandler>();
+    //    eventBus.AddSubscription<OrderStatusChangedToStockConfirmedIntegrationEvent, OrderStatusChangedToStockConfirmedIntegrationEventHandler>();
+    //    eventBus.AddSubscription<OrderStatusChangedToShippedIntegrationEvent, OrderStatusChangedToShippedIntegrationEventHandler>();
+    //    eventBus.AddSubscription<OrderStatusChangedToCancelledIntegrationEvent, OrderStatusChangedToCancelledIntegrationEventHandler>();
+    //    eventBus.AddSubscription<OrderStatusChangedToSubmittedIntegrationEvent, OrderStatusChangedToSubmittedIntegrationEventHandler>();
+    //}
+
+    //public static void AddAuthenticationServices(this IHostApplicationBuilder builder)
+    //{
+    //    var configuration = builder.Configuration;
+    //    var services = builder.Services;
+
+    //    JsonWebTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+    //    var identityUrl = configuration.GetRequiredValue("IdentityUrl");
+    //    var callBackUrl = configuration.GetRequiredValue("CallBackUrl");
+    //    var sessionCookieLifetime = configuration.GetValue("SessionCookieLifetimeMinutes", 60);
+
+    //    // Add Authentication services
+    //    services.AddAuthorization();
+    //    services.AddAuthentication(options =>
+    //    {
+    //        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    //        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    //    })
+    //    .AddCookie(options => options.ExpireTimeSpan = TimeSpan.FromMinutes(sessionCookieLifetime))
+    //    .AddOpenIdConnect(options =>
+    //    {
+    //        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    //        options.Authority = identityUrl;
+    //        options.SignedOutRedirectUri = callBackUrl;
+    //        options.ClientId = "webapp";
+    //        options.ClientSecret = "secret";
+    //        options.ResponseType = "code";
+    //        options.SaveTokens = true;
+    //        options.GetClaimsFromUserInfoEndpoint = true;
+    //        options.RequireHttpsMetadata = false;
+    //        options.Scope.Add("openid");
+    //        options.Scope.Add("profile");
+    //        options.Scope.Add("orders");
+    //        options.Scope.Add("basket");
+    //    });
+
+    //    // Blazor auth services
+    //    services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+    //    services.AddCascadingAuthenticationState();
+    //}
+
+    public static async Task<string?> GetBuyerIdAsync(this AuthenticationStateProvider authenticationStateProvider)
+    {
+        var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+        return user.FindFirst("sub")?.Value;
+    }
+
+    public static async Task<string?> GetUserNameAsync(this AuthenticationStateProvider authenticationStateProvider)
+    {
+        var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+        return user.FindFirst("name")?.Value;
+    }
+}
