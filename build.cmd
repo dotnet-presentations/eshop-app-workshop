5757febac19062d31d7a9eae33f1fe1c8db47c68
Damian Edwards <damian@damianedwards.com>
2024-01-25 22:04:59 -0800

    Add CI workflow

---
+++ build.cmd
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+dotnet workload update --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json
+dotnet workload install aspire --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json
+
+dotnet build .\build\Build.proj
