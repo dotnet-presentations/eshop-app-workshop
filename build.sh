03d910c2d04f98dfb1d596865a2c11ff3e4cbc55
Damian Edwards <damian@damianedwards.com>
2024-01-25 22:09:12 -0800

    Mark build.sh executable

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 dotnet workload update --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json
 dotnet workload install aspire --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json
 
+
 dotnet build ./build/Build.proj
