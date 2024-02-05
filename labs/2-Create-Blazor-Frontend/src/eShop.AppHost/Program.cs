9ad0d193f80319089179d1803b7696910fa15144
Damian Edwards <damian@damianedwards.com>
2024-02-05 15:45:05 +1100

    Add lab 2 src starting point

---
+++ labs/2-Create-Blazor-Frontend/src/eShop.AppHost/Program.cs
@@ -0,0 +1,27 @@
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+// Databases
+
+var postgres = builder.AddPostgres("postgres").WithPgAdmin();
+var catalogDb = postgres.AddDatabase("CatalogDB");
+
+// DB Manager Apps
+
+builder.AddProject<Catalog_Data_Manager>("catalog-db-mgr")
+    .WithReference(catalogDb);
+
+
+// API Apps
+
+var catalogApi = builder.AddProject<Catalog_API>("catalog-api")
+    .WithReference(catalogDb);
+
+// Apps
+
+
+// Inject assigned URLs for Catalog API
+catalogApi.WithEnvironment("CatalogOptions__PicBaseAddress", () => catalogApi.GetEndpoint("http").UriString);
+
+builder.Build().Run();
