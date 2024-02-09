6185162390f19dd9f6af930e85e4417759883368
Damian Edwards <damian@damianedwards.com>
2024-02-09 17:06:45 +1100

    Add lab 4 end & remove unused config values

---
+++ labs/4-Add-Shopping-Basket/end/eShop.AppHost/KeycloakResource.cs
@@ -0,0 +1,62 @@
+﻿using Aspire.Hosting.Publishing;
+
+namespace Aspire.Hosting;
+
+internal static class KeycloakHostingExtensions
+{
+    private const int DefaultContainerPort = 8080;
+
+    public static IResourceBuilder<KeycloakContainerResource> AddKeycloakContainer(
+        this IDistributedApplicationBuilder builder,
+        string name,
+        int? port = null,
+        string? tag = null)
+    {
+        var keycloakContainer = new KeycloakContainerResource(name);
+
+        return builder
+            .AddResource(keycloakContainer)
+            .WithAnnotation(new ContainerImageAnnotation { Registry = "quay.io", Image = "keycloak/keycloak", Tag = tag ?? "latest" })
+            .WithHttpEndpoint(hostPort: port, containerPort: DefaultContainerPort)
+            .WithEnvironment("KEYCLOAK_ADMIN", "admin")
+            .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", "admin")
+            .WithArgs("start-dev")
+            .WithManifestPublishingCallback(context => WriteKeycloakContainerToManifest(context, keycloakContainer));
+    }
+
+    public static IResourceBuilder<KeycloakContainerResource> ImportRealms(this IResourceBuilder<KeycloakContainerResource> builder, string source)
+    {
+        builder
+            .WithVolumeMount(source, "/opt/keycloak/data/import", VolumeMountType.Bind)
+            .WithAnnotation(new ExecutableArgsCallbackAnnotation(args =>
+            {
+                args.Clear();
+                args.Add("start-dev");
+                args.Add("--import-realm");
+            }));
+
+        return builder;
+    }
+
+    private static void WriteKeycloakContainerToManifest(ManifestPublishingContext context, KeycloakContainerResource resource)
+    {
+        var manifestResource = new KeycloakContainerResource(resource.Name);
+
+        foreach (var annotation in resource.Annotations)
+        {
+            if (annotation is not ExecutableArgsCallbackAnnotation)
+            {
+                manifestResource.Annotations.Add(annotation);
+            }
+        }
+
+        // Set the container entry point to 'start' instead of 'start-dev'
+        manifestResource.Annotations.Add(new ExecutableArgsCallbackAnnotation(args => args.Add("start")));
+
+        context.WriteContainer(resource);
+    }
+}
+
+internal class KeycloakContainerResource(string name) : ContainerResource(name), IResourceWithServiceDiscovery
+{
+}
---
+++ labs/4-Add-Shopping-Basket/end/eShop.AppHost/Program.cs
@@ -0,0 +1,40 @@
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+// Databases
+
+var postgres = builder.AddPostgres("postgres").WithPgAdmin();
+var catalogDb = postgres.AddDatabase("CatalogDB");
+
+// Identity Providers
+
+var idp = builder.AddKeycloakContainer("idp", tag: "23.0")
+    .ImportRealms("../Keycloak/data/import");
+
+// DB Manager Apps
+
+builder.AddProject<Catalog_Data_Manager>("catalog-db-mgr")
+    .WithReference(catalogDb);
+
+// API Apps
+
+var catalogApi = builder.AddProject<Catalog_API>("catalog-api")
+    .WithReference(catalogDb);
+
+// Apps
+
+var webApp = builder.AddProject<WebApp>("webapp")
+    .WithReference(catalogApi)
+    .WithReference(idp)
+    // Force HTTPS profile for web app (required for OIDC operations)
+    .WithLaunchProfile("https");
+
+// Inject the project URLs for Keycloak realm configuration
+idp.WithEnvironment("WEBAPP_HTTP", () => webApp.GetEndpoint("http").UriString);
+idp.WithEnvironment("WEBAPP_HTTPS", () => webApp.GetEndpoint("https").UriString);
+
+// Inject assigned URLs for Catalog API
+catalogApi.WithEnvironment("CatalogOptions__PicBaseAddress", () => catalogApi.GetEndpoint("http").UriString);
+
+builder.Build().Run();
