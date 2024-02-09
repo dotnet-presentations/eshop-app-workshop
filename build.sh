38532a239e6d4fb104e7dab639a92758c6293b98
Damian Edwards <damian@damianedwards.com>
2024-02-09 15:22:56 +1100

    Update build scripts & README to handle installing Aspire workload (#16)
    * Update build scripts & README to handle installing Aspire workload
    
    * Executable!

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-# Daily builds
-#dotnet workload update --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json
-#dotnet workload install aspire --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json
-
-# Release candidate builds
-dotnet workload update --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-tools/nuget/v3/index.json
-dotnet workload install aspire --skip-sign-check --source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet-tools/nuget/v3/index.json
+# Install the latest version of the Aspire workload from the release branches
+./build/installLatestFromReleaseBranch.sh
 
 dotnet build ./build/Build.proj
