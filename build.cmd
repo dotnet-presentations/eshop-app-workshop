caf3ec803e4ac5f1361fbcb1592b592258c26a85
David Fowler <davidfowl@gmail.com>
2024-02-28 22:29:26 -0800

    Remove release feed download of aspire bits from build

--- build.cmd
+++ build.cmd
@@ -1,6 +1,3 @@
 @ECHO OFF
 
-:: Install the latest version of the Aspire workload from the release branches
-powershell -ExecutionPolicy Bypass -NoProfile -Command "& '.\build\installLatestFromReleaseBranch.ps1'"
-
 dotnet build .\build\Build.proj
--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-# Install the latest version of the Aspire workload from the release branches
-./build/installLatestFromReleaseBranch.sh
-
 dotnet build ./build/Build.proj
