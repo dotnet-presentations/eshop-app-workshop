d4dfff9bd192777c02469ae92df02d0f469c7a44
Damian Edwards <damian@damianedwards.com>
2024-04-24 13:57:58 -0700

    Update WebAppTests.cs

--- src/IntegrationTests/WebAppTests.cs
+++ src/IntegrationTests/WebAppTests.cs
@@ -11,13 +11,23 @@ public class WebAppTests
         var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.eShop_AppHost>();
         appHost.Services.ConfigureHttpClientDefaults(client =>
         {
-            client.AddStandardResilienceHandler();
+            client.AddStandardResilienceHandler(options =>
+            {
+                options.TotalRequestTimeout.Timeout = TimeSpan.FromSeconds(300);
+                options.AttemptTimeout.Timeout = TimeSpan.FromSeconds(30);
+                options.Retry.BackoffType = Polly.DelayBackoffType.Constant;
+                options.Retry.Delay = TimeSpan.FromSeconds(5);
+                options.Retry.MaxRetryAttempts = 100;
+                options.CircuitBreaker.SamplingDuration = TimeSpan.FromSeconds(60);
+            });
         });
+
         await using var app = await appHost.BuildAsync();
         await app.StartAsync();
-        var urls = new[] { "/alive", "/health", "/" };
 
         var httpClient = app.CreateHttpClient("webapp");
+
+        var urls = new[] { "/alive", "/health", "/" };
         foreach (var url in urls)
         {
             var response = await httpClient.GetAsync(url);
